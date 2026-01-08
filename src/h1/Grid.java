c734522c4f83d5ffd339d5cb5215754200e60d60
Leo Bernsmann <leo@MacBook-Air-von-Leo.local>
2026-01-08 14:13:19 +0100

    23

--- src/h1/Grid.java
+++ src/h1/Grid.java
@@ -1,5 +1,69 @@
 package h1;
 
-public class Grid {
+	public class Grid {
 
-}
+	    private Cell[][] gridArray;
+
+	    public Cell[][] getGridArray() {
+	        return gridArray;
+	    }
+
+	    public void setGridArray(Cell[][] gridArray) {
+	        this.gridArray = gridArray;
+	    }
+
+	    public Grid(Cell[] cells, int gridRows, int gridCols) {
+
+	        gridArray = new Cell[gridRows][gridCols];
+
+	        for (int r = 0; r < gridRows; r++) {
+	            for (int c = 0; c < gridCols; c++) {
+	                gridArray[r][c] = new Cell(r, c, false);
+	            }
+	        }
+	        
+	        for (int i = 0; i < cells.length; i++) {
+	            int r = cells[i].getIndexRow();
+	            int c = cells[i].getIndexCol();
+
+	            if (r >= 0 && r < gridRows && c >= 0 && c < gridCols) {
+	                gridArray[r][c].setAlive(true);
+	            }
+	        }
+	        
+	        for (int r = 0; r < gridRows; r++) {
+	            for (int c = 0; c < gridCols; c++) {
+	                gridArray[r][c].countLivingNeighbors(gridArray);
+	            }
+	        }
+	    }
+
+	    public void computeNextGen() {
+	        int rows = gridArray.length;
+	        int cols = gridArray[0].length;
+
+	        for (int r = 0; r < rows; r++) {
+	            for (int c = 0; c < cols; c++) {
+	                gridArray[r][c].countLivingNeighbors(gridArray);
+	            }
+	        }
+
+	        for (int r = 0; r < rows; r++) {
+	            for (int c = 0; c < cols; c++) {
+	                gridArray[r][c].setAlive(gridArray[r][c].isAliveNextGen());
+	            }
+	        }
+
+	        for (int r = 0; r < rows; r++) {
+	            for (int c = 0; c < cols; c++) {
+	                gridArray[r][c].countLivingNeighbors(gridArray);
+	            }
+	        }
+	    }
+
+	    public void computeGeneration(int n) {
+	        for (int i = 0; i < n; i++) {
+	            computeNextGen();
+	        }
+	    }
+	}
